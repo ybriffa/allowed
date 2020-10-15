5025caad4bbf08333fb2c2284f6a3a910a05271e
Yannick Briffa <yannick.briffa@corp.ovh.com>
2020-10-15 13:16:37 +0000

    first implementation

---
+++ isallow.go
@@ -0,0 +1,123 @@
+package isallow
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const (
+	// AllowedTag is the structure tag to set to validate the structure
+	AllowedTag = "allowed"
+)
+
+type explorer struct {
+	value string
+	ptrs  map[reflect.Type]map[uintptr]reflect.Value
+}
+
+// Check defines whether the given value is allowed in the structure fields.
+func Check(value string, i interface{}) error {
+	e := explorer{
+		ptrs:  map[reflect.Type]map[uintptr]reflect.Value{},
+		value: strings.ToLower(value),
+	}
+	return e.explore(reflect.ValueOf(i))
+}
+
+func explorable(t reflect.Type) bool {
+	switch t.Kind() {
+	case reflect.Struct:
+		return true
+	case reflect.Ptr, reflect.Array, reflect.Slice:
+		return explorable(t.Elem())
+	case reflect.Map:
+		// XXX: we are not handling map's keys for now
+		// Can be accessed with t.Key()
+		return explorable(t.Elem())
+	}
+	return false
+}
+
+func (e *explorer) explore(v reflect.Value) error {
+	if !explorable(v.Type()) {
+		return fmt.Errorf("data of type %s cannot be checked", v.Type())
+	}
+
+	return e.validate(v)
+}
+
+func (e *explorer) allowed(content string) bool {
+	for _, s := range strings.Split(content, ",") {
+		if strings.ToLower(s) == e.value {
+			return true
+		}
+	}
+	return false
+}
+
+func (e *explorer) validate(v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return nil
+	case reflect.Array, reflect.Slice:
+		if v.IsNil() {
+			return nil
+		}
+		for i := 0; i < v.Len(); i++ {
+			if err := e.validate(v.Index(i)); err != nil {
+				return fmt.Errorf("item %d: %s", i, err)
+			}
+		}
+		return nil
+	case reflect.Map:
+		if v.IsNil() {
+			return nil
+		}
+
+		for _, key := range v.MapKeys() {
+			// XXX: not checking map's keys for now
+			if err := e.validate(v.MapIndex(key)); err != nil {
+				return fmt.Errorf("key %s: %s", key, err)
+			}
+		}
+		return nil
+	}
+
+	for v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface {
+		if v.IsNil() {
+			return nil
+		}
+		v = v.Elem()
+	}
+
+	return e.validateStruct(v)
+}
+
+func (e *explorer) validateStruct(v reflect.Value) error {
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		fieldInfos := t.Field(i)
+		fieldValue := v.Field(i)
+
+		allowed := e.allowed(fieldInfos.Tag.Get(AllowedTag))
+		if !allowed && isSet(fieldValue) {
+			return fmt.Errorf("field %q: not allowed to set", fieldInfos.Name)
+		}
+
+		if allowed && explorable(fieldValue.Type()) {
+			err := e.validate(fieldValue)
+			if err != nil {
+				return fmt.Errorf("field %q: %s", fieldInfos.Name, err)
+			}
+		}
+	}
+	return nil
+}
+
+func isSet(v reflect.Value) bool {
+	if v.Kind() == reflect.Invalid {
+		return false
+	}
+	return !v.IsZero()
+}
---
+++ isallow_test.go
@@ -0,0 +1,163 @@
+package isallow
+
+import (
+	"testing"
+)
+
+type first struct {
+	A string `allowed:"POST"`
+	B string
+}
+
+type second struct {
+	C string `allowed:"POST"`
+}
+
+type third struct {
+	first  *first `allowed:"POST"`
+	second *second
+}
+
+func TestAllowed(t *testing.T) {
+	tests := []struct {
+		tagValue    string
+		value       interface{}
+		shouldError bool
+	}{
+		// 0 - Invalid data input
+		{
+			tagValue:    "POST",
+			value:       "42",
+			shouldError: true,
+		},
+
+		// 1 - Valid basic case
+		{
+			tagValue: "POST",
+			value: first{
+				A: "toto",
+			},
+			shouldError: false,
+		},
+
+		// 2 - Invalid basic case
+		{
+			tagValue: "POST",
+			value: first{
+				B: "toto",
+			},
+			shouldError: true,
+		},
+
+		// 3 - Basic pointer case
+		{
+			tagValue: "POST",
+			value: &first{
+				A: "toto",
+			},
+			shouldError: false,
+		},
+
+		// 4 - Invalid Array
+		{
+			tagValue: "POST",
+			value: []string{
+				"toto",
+			},
+			shouldError: true,
+		},
+
+		// 5 - Valid Array with pointer to struct
+		{
+			tagValue: "POST",
+			value: []*first{
+				&first{
+					A: "toto",
+				},
+			},
+			shouldError: false,
+		},
+
+		// 6 - Valid Array with pointer to struct with invalid type
+		{
+			tagValue: "POST",
+			value: []*first{
+				&first{
+					B: "toto",
+				},
+			},
+			shouldError: true,
+		},
+
+		// 7 - Invalid map Type
+		{
+			tagValue: "POST",
+			value: map[string]string{
+				"key": "value",
+			},
+			shouldError: true,
+		},
+
+		// 8 - Valid map
+		{
+			tagValue: "POST",
+			value: map[string]*first{
+				"key": &first{
+					A: "toto",
+				},
+			},
+			shouldError: false,
+		},
+
+		// 9 - Valid map and invalid field set
+		{
+			tagValue: "POST",
+			value: map[string]*first{
+				"key": &first{
+					B: "toto",
+				},
+			},
+			shouldError: true,
+		},
+
+		// 10 - Valid fields in struct
+		{
+			tagValue: "POST",
+			value: &third{
+				first: &first{
+					A: "toto",
+				},
+			},
+			shouldError: false,
+		},
+
+		// 10 - Invalid fields in field struct
+		{
+			tagValue: "POST",
+			value: &third{
+				first: &first{
+					B: "toto",
+				},
+			},
+			shouldError: true,
+		},
+
+		// 11 - Invalid fields set even if child field is correct
+		{
+			tagValue: "POST",
+			value: &third{
+				second: &second{
+					C: "toto",
+				},
+			},
+			shouldError: true,
+		},
+	}
+
+	for i, test := range tests {
+		result := Check(test.tagValue, test.value)
+		if test.shouldError != (result != nil) {
+			t.Fatalf("test #%d failed: expected to crash %v and got error: %v", i, test.shouldError, result)
+		}
+	}
+}
